182e1d528cd39bcc8ab3ec0b800dc2a201947548
shoibloya <shoib.loya@gmail.com>
2024-12-11 23:07:22 +0800

    remove sensitive

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,13 +6,13 @@ import { getAuth } from 'firebase/auth'; // Import Firebase Auth
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD-rloy5mH7b7dYXOw-OVX0HlGxpDrVq4k",
-  authDomain: "nuggt-research.firebaseapp.com",
-  projectId: "nuggt-research",
-  storageBucket: "nuggt-research.firebasestorage.app",
-  messagingSenderId: "63008997915",
-  appId: "1:63008997915:web:4c79caaac85bb479322ebe",
-  measurementId: "G-G4M9L745JM"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
